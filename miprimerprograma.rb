c66a7b37b14dd94fa4432b6b52e90fcb0f6df16a
teslarobot! <teslarobot@conejonipples.local>
2015-01-21 18:37:39 -0500

    holas

--- miprimerprograma.rb
+++ miprimerprograma.rb
@@ -1,14 +1,27 @@
-puts "Hola git"
+=begin
+puts "Qué quieres hacer?"
+puts "(1) sumar (2) restar (3) multiplicar (4) dividir (5) nada" 
+operacion = gets.chomp.to_i
 
-a = 8
-b = 8
+puts "dame el primer numero"
+numero1 = gets.chomp.to_i
 
-if a > b
-	puts "a es mayor que b"
-elsif a == b
-	puts "somos iguales"
-else 
-	puts "b es mayor que a"
+puts "dame el segundo numero"
+numero2 = gets.chomp.to_i
+
+if operacion == 1 
+	puts "el resultado es #{numero1 + numero2}" 
+resultado=numero1+numero2
+elsif operacion == 2
+	puts "el resultado es #{numero1 - numero2}" 
+
+elsif operacion == 3 
+	puts "el resultado es #{numero1 * numero2} " 
+
+elsif operacion == 4 
+	puts "el resultado es #{numero1 / numero2}"  
+else
+	puts "No es una operacion valida"	
 end
-
+=end
 
